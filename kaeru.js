7d05397acb42f9964969c0dda8831f5be06a72b4
knz21 <cismollco@gmail.com>
2016-10-19 20:11:53 +0900

    storage area

--- kaeru.js
+++ kaeru.js
@@ -7,7 +7,7 @@ var historyIndex = 0;
 var BACKUP_FILE_PREFIX = 'kaeru_backup_';
 var BACKUP_SEPARATOR = '\n\n\n---------------------\n\n\n';
 var MIN_CHILD_HEIGHT = 72;
-var MIN_CHILD_WIDTH = 256;
+var MIN_CHILD_WIDTH = 160;
 
 (function () {
     bindActions();
